b3b751790eb5ee99a0c1f2ce6f0997140abf7705
Stephen Snyder <ssnyder555@live.com>
2018-10-12 16:08:05 -0500

    connected up again

--- destinations/controllers/dc.js
+++ destinations/controllers/dc.js
@@ -1,11 +1,27 @@
+
 const express = require('express');
 
 const router = express.Router();
 
-const Destinations = require('./model/destinations')
+const Destinations = require('../models/destinations')
 
 router.get('/', (req,res) => {
   res.send('Its all working now, time to go home')
+})
+
+router.post('/', (req,res) => {
+  Destinations.create({
+    name: 'Tulum',
+    fly: true,
+    climate: 'hot',
+    image: 'https://exp.cdn-hotels.com/hotels/5000000/4520000/4515800/4515733/4c6016ed_z.jpg'
+  }, (err, createDestination) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(createDestination);
+    }
+  })
 })
 
 module.exports = router;
--- destinations/server.js
+++ destinations/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
-const methodOVerride = require('method-override');
-const bodyParser = require('body-parser');
+
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 require('./db/db');
 
-const destinationsController = require('./controllers/dc');
+const destinationsController = require('./controllers/dc')
 
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(methodOVerride('_method'));
 app.use('/destinations', destinationsController);
 
+// app.get('/', (req,res) => {
+//   res.send('It is Aliveeeeeee');
+// })
 
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+  console.log('App is alive');
+})
