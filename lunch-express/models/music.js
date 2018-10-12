49df584f4cd5d9cfe02a1787308cb5dab8980d21
Stephen Snyder <ssnyder555@live.com>
2018-10-12 14:25:52 -0500

    playground

---
+++ lunch-express/models/music.js
@@ -0,0 +1,19 @@
+const music = [
+  {
+    artiest: 'Mac Miller',
+    genre: 'rap',
+    goodWithBass: true
+  };
+
+  {
+    artiest: 'Bethoven',
+    genre: 'classical',
+    goodWithBass: false
+  };
+  {
+    artiest: 'Kanye West',
+    genre: 'rap',
+    goodWithBass: true
+  };
+];
+module.exports = music;
