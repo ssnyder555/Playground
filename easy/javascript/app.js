6a886ce5ae793df74ae05cf612364ccf0552cd5c
Stephen Snyder <ssnyder555@live.com>
2018-10-18 09:23:42 -0500

    extra credit

---
+++ easy/javascript/app.js
@@ -0,0 +1,13 @@
+// console.log('working');
+
+// function buttonClick() {
+// let myButton = document.getElementByClassName("bo");
+// // mybutton.onclick = showAlert;
+// console.log("workin");
+// myButton.click()
+//
+//
+// };
+function clickAlert() {
+  alert("Alert!");
+}
